71c11cd9427284ddbd053d59c41d19a773331e46
Asmayaseen <asmayaseen9960@gmail.com>
2024-12-09 19:44:08 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Card from "@/components/Card";
+import Essentials from "@/components/essentials";
+import Featured from "@/components/Featured";
+import Gear from "@/components/Gear";
+import HeroSection from "@/components/HeroSection";
+import Stan from "@/components/stan";
+
+export default function Home() {
+  return (
+    <>
+ <HeroSection/>
+ <Card/>
+ <Featured/>
+ <Gear/>
+ <Stan/>
+ <Essentials/>
+ </>
+  );
+}
